4f5ea46c60cd1b323dac381713b3f12ae037ef5d
hardi1807 <hsathvara18@gmail.com>
2026-07-04 16:11:18 +0530

    updated

--- CareSync/backend/middleware/authDoctor.js
+++ CareSync/backend/middleware/authDoctor.js
@@ -1,39 +1,33 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const authDoctor = async (req, res, next) => {
-    try {
+  try {
+    const dToken = req.headers.dtoken;
 
-        const dToken = req.headers.dtoken;
+    console.log("DOCTOR TOKEN:", dToken);
 
-        console.log("DOCTOR TOKEN:", dToken);
-
-        if (!dToken) {
-            return res.json({
-                success: false,
-                message: 'Not Authorized'
-            });
-        }
-
-        const decoded = jwt.verify(
-            dToken,
-            process.env.JWT_SECRET
-        );
-
-        console.log("DECODED:", decoded);
-
-        req.user = decoded;
-
-        next();
-
-    } catch (error) {
-
-        console.log("AUTH ERROR:", error);
-
-        res.json({
-            success: false,
-            message: error.message
-        });
+    if (!dToken) {
+      return res.json({
+        success: false,
+        message: "Not Authorized",
+      });
     }
+
+    const decoded = jwt.verify(dToken, process.env.JWT_SECRET);
+
+    console.log("DECODED:", decoded);
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    console.log("AUTH ERROR:", error);
+
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export default authDoctor;
